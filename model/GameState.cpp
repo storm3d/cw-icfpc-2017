d245b1ae902a52700be143316137fb80200f9ec8
Victor Sergienko <singalen@gmail.com>
2017-08-04 13:50:32 -0700

    Graph representation: Incidence list/hashmap

--- model/GameState.cpp
+++ model/GameState.cpp
@@ -1,21 +1,78 @@
-//
-// Created by Dmytro Bochorishvili on 8/4/17.
-//
-
+#include <cassert>
 #include "GameState.h"
-
 #include "json.hpp"
 
 // for convenience
 using json = nlohmann::json;
 
 GameState::GameState(std::istream &in) {
-    json j;
-    in >> j;
-    playersNum = j["punters"];
-
+	json j;
+	in >> j;
+	playersNum = j["punters"];
 }
 
 vert_t GameState::getPlayersNum() {
-    return playersNum;
+	return playersNum;
+}
+
+vert_t GameState::getSitesNum() {
+	return incidence_list.size();
+}
+
+vert_t GameState::getMinesNum() {
+	return mines.size();
+}
+
+bool GameState::isMine(vert_t i) {
+	return incidence_list[i].is_mine;
+}
+
+bool GameState::isEdge(vert_t from, vert_t to) {
+	if (from > to)
+	{
+		vert_t tmp = from;
+		from = to;
+		to = tmp;
+	}
+
+	assert(to < incidence_list.size());
+
+	auto it = incidence_list[from].edges.find(to);
+	return it != incidence_list[from].edges.end();
+}
+
+punter_t GameState::getClaimerId(vert_t from, vert_t to) {
+	if (from > to)
+	{
+		vert_t tmp = from;
+		from = to;
+		to = tmp;
+	}
+
+	assert(to < incidence_list.size());
+
+	auto it = incidence_list[from].edges.find(to);
+	if (it != incidence_list[from].edges.end())
+	{
+		return it->second;
+	}
+
+	return 0;
+}
+
+void GameState::claimEdge(vert_t from, vert_t to, punter_t punter) {
+	if (from > to)
+	{
+		vert_t tmp = from;
+		from = to;
+		to = tmp;
+	}
+
+	assert(to < incidence_list.size());
+
+	auto it = incidence_list[from].edges.find(to);
+	if (it != incidence_list[from].edges.end())
+	{
+		incidence_list[from].edges[to] = punter;
+	}
 }
--- model/GameState.h
+++ model/GameState.h
@@ -1,17 +1,20 @@
 #ifndef CW_ICFPC_2017_STATE_H
 #define CW_ICFPC_2017_STATE_H
 
-
 #include <vector>
+#include <unordered_set>
+#include <unordered_map>
 #include <iostream>
+#include <cstdint>
 
-typedef unsigned short vert_t;
+typedef uint64_t vert_t;
+typedef uint64_t punter_t;
+
 
 class GameState {
-
 public:
-
     GameState(std::istream &in);
+	void Serialize(std::ostream &in);
 
     vert_t getSitesNum();
     vert_t getPlayersNum();
@@ -19,28 +22,27 @@ public:
     int getCurrentTurnNum();
 
     bool isEdge(vert_t i, vert_t j);
-    bool isClaimed(vert_t i, vert_t j);
-    
-    int getClaimerId(vert_t i, vert_t j);
+	punter_t getClaimerId(vert_t i, vert_t j);
 
-    void claimEdge(vert_t i, vert_t j);
+    void claimEdge(vert_t i, vert_t j, punter_t punter);
 
     bool isMine(vert_t i);
 
-    void Serialize(std::ostream &in);
+private:
+	struct VertexIncidence
+	{
+		bool is_mine;
+		std::unordered_map<vert_t, punter_t> edges;
+	};
 
-protected:
-
-    std::vector<vert_t> vertices;
-    std::vector<vert_t> mines;
+	std::vector<VertexIncidence> incidence_list;
+	std::unordered_set<vert_t> mines;
 
     vert_t playersNum;
 
     int punterId;
 
     int currentTurn;
-
 };
-
 
 #endif //CW_ICFPC_2017_STATE_H
