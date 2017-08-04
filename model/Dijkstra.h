9fc0062d4570168aade5bfb95a5c6dd996bf6478
Dmytro Bochorishvili <dbochorishvili@smartling.com>
2017-08-05 00:23:44 +0300

    add adjacency list and min distance to parameters of Dijkstra function

---
+++ model/Dijkstra.h
@@ -0,0 +1,9 @@
+#ifndef CW_ICFPC_2017_DIJKSTRA_H
+#define CW_ICFPC_2017_DIJKSTRA_H
+
+#include <vector>
+#include "GameState.h"
+
+void Dijkstra(vert_t s, vert_t n, std::vector<std::vector<vert_t>> adj, vert_t dis[]);
+
+#endif //CW_ICFPC_2017_DIJKSTRA_H
