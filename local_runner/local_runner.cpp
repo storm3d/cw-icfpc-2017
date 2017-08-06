c4562b0cd1312c017950612f130c01319eec651d
e-vasyl <e.vasyl@gmail.com>
2017-08-06 15:47:21 +0300

    add more params to local_runner, make it 1 step after startup

--- local_runner/local_runner.cpp
+++ local_runner/local_runner.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <memory>
 #include <json.hpp>
 
@@ -7,9 +8,9 @@
 
 using json = nlohmann::json;
 
-std::string punter_name = "punter";
-std::string map_name = "sample";
-
+std::vector<std::string> g_PunterNames = {};
+std::string g_MapName = "sample";
+int g_Punters = 0;
 
 json read_file(const std::string & filename)
 {
@@ -54,80 +55,171 @@ std::string wrap_json(const json& input)
     return data + input_string;
 }
 
-std::string call_punter(const json& input_json)
+std::string call_punter(const int punter, const json& input_json)
 {
     std::string cmd;
 #ifdef WINVER
     cmd = "cmd /c ";
-    cmd += punter_name;
+    cmd += g_PunterNames[punter];
     cmd += ".exe";
 #else
     cmd = "./";
-    cmd += punter_name;
+    cmd += g_PunterNames[punter];
 #endif // WINDOWS
     json handshake;
-    handshake["you"] = "cw";
+    handshake["you"] = "SERVER";
     std::string message = wrap_json(handshake) + wrap_json(input_json);
     return exec(cmd, message);
 }
 
-json startup(int player, int players_count, const std::string & filename)
+json create_startup(int punter, int punters, const json & map)
 {
     json j;
-    j["punter"] = player;
-    j["punters"] = players_count;
-    j["map"] = read_file(filename);
+    j["punter"] = punter;
+    j["punters"] = punters;
+    j["map"] = map;
     return j;
 }
 
-json pass_move(int player)
+json create_pass_move(int punter)
 {
   json j, j2;
-  j2["punter"] = player;
+  j2["punter"] = punter;
   j["pass"] = j2;
   return j;
 }
 
-json claim_move(int player, int src, int dst)
+json parse_response(const std::string & response)
 {
-  json j, j2;
-  j2["punter"] = player;
-  j2["source"] = src;
-  j2["target"] = dst;
-  j["claim"] = j2;
+  unsigned long len;
+  char ch;
+  std::istringstream in(response);
+  
+  in>>len>>ch;
+  for(int i=len;i-->0;) in>>ch;
+  in>>len>>ch;
+  json j;
+  j << in;
   return j;
 }
 
-/*json parse_response(std::stream & istream)
-{
-  
-}*/
+struct runner_state{
+  std::vector<json> m_States;
+  std::vector<json> m_Moves;
+  int turns;
+
+  void init (int n, const json& map)
+  {
+    for (int i = 0; i < n; i++)
+    {
+      json j;
+      j["state"] = i;
+      m_States.push_back(j);
+      m_Moves.push_back(create_pass_move(i));
+    }
+    turns = 1;
+  }
+
+  void process_response(int punter, const json& data)
+  {
+    if (data.find("ready") == data.end())
+    {
+      json j;
+      if (data.find("pass") != data.end())
+      {
+        j["pass"] = data["pass"];
+      }
+      else if (data.find("claim") != data.end())
+      {
+        j["claim"] = data["claim"];
+      }
+      else if (data.find("splurge") != data.end())
+      {
+        j["splurge"] = data["splurge"];
+      }
+      else
+        std::cerr << "ERROR!!!" << std::endl;
+
+      m_Moves[punter] = j;
+    }
+    
+    m_States[punter]["state"] = data["state"];
+
+  }
+
+  json m_Move;
+  void finish_process()
+  {
+    json moves = m_Moves;
+    m_Move["move"]["moves"] = moves;
+  }
+
+  json create_move_query(int punter)
+  {
+    m_Move["state"] = m_States[punter];
+  }
+
+} g_State;
 
 int main(int argc, char *argv[]) {
       int opt;
-      while ((opt = getopt(argc, argv, "p:m:")) != -1) {
+      while ((opt = getopt(argc, argv, "p:m:n:")) != -1) {
         switch (opt) {
         case 'p':
-            punter_name = optarg;
+            g_PunterNames.push_back(optarg);
             break;
         case 'm':
-            map_name = optarg;
+            g_MapName = optarg;
+            break;
+        case 'n':
+            g_Punters = atoi(optarg);
             break;
         default: /* '?' */
-            fprintf(stderr, "Usage: %s [-p punter] [-m map_name]\n",
+            fprintf(stderr, "Usage: %s [-m g_MapName] [-n punters] [-p punter_name]\n",
                     argv[0]);
             exit(EXIT_FAILURE);
         }
     }
 
-    //
+    // default punter name
+    if (g_PunterNames.size() < 1) g_PunterNames.push_back("punter");
+    // calc punters
+    if (g_Punters < 2)
+      g_Punters = 2;
+    
+    // spread values
+    while (g_PunterNames.size() < g_Punters)
+      g_PunterNames.push_back(g_PunterNames[g_PunterNames.size()-1]);
+    
     std::cout << "Local runner for punters" << std::endl;
     std::string map = "../maps/";
-    map += map_name;
+    map += g_MapName;
     map += ".json";
+    json map_data = read_file(map);
     //std::cout<< map << "\n";
-    json test = startup(1, 2, map);
+    
+    g_State.init(g_Punters, map_data);
+    
+    for (int i = 0; i < g_Punters; i++)
+    {
+      json test = create_startup(i, g_Punters, map_data);
+      std::string response = call_punter(i, test);
+        std::cout<<response<<std::endl;
+      g_State.process_response(i, parse_response(response));
+    }
+    g_State.finish_process();
 
-    std::cout << call_punter(test) << std::endl;
+    //std::cout<<g_State.m_Move.dump(1)<<std::endl;
+    
+    for(int turn = 0; turn < g_State.turns; turn++)
+    {
+      for (int i = 0; i < g_Punters; i++)
+      {
+        json test = g_State.create_move_query(i);
+        std::string response = call_punter(i, test);
+        g_State.process_response(i, parse_response(response));
+      }
+      g_State.finish_process();
+    }
     return 0;
 }
