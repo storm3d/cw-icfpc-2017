c8f83937e3418bad0a64a8c0029617ff67147cfb
Ihor Omelchenko <megapixel23@gmail.com>
2017-08-07 13:11:07 +0300

    Submission build script was fixed.

--- submission.sh
+++ submission.sh
@@ -1,5 +1,6 @@
+reg_id=19902dd7-1ef6-43c8-8ddd-17d67806316f
 rm ./bin/punter
-mv icfp-19902dd7-1ef6-43c8-8ddd-17d67806316f.tar.gz $(mktemp old/icfp-19902dd7-1ef6-43c8-8ddd-17d67806316f.tar.gz_XXXX)
+mv icfp-$reg_id.tar.gz $(mktemp old/icfp-$reg_id.tar.gz_XXXX)
 cmake .
 make
-tar -czvf icfp-19902dd7-1ef6-43c8-8ddd-17d67806316f.tar.gz /bin/punter
+tar -czvf icfp-$reg_id.tar.gz ./bin/punter
