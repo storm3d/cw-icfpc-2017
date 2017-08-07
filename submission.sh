60306080f9e2c556383e798ad51863aef57abc78
Ihor Omelchenko <megapixel23@gmail.com>
2017-08-07 13:47:38 +0300

    Needed files for submission were added.

--- submission.sh
+++ submission.sh
@@ -1,6 +1,13 @@
 reg_id=19902dd7-1ef6-43c8-8ddd-17d67806316f
-rm ./bin/punter
 mv icfp-$reg_id.tar.gz $(mktemp old/icfp-$reg_id.tar.gz_XXXX)
+
 cmake .
 make
-tar -czvf icfp-$reg_id.tar.gz ./bin/punter
+
+rm /tmp/src
+mkdir /tmp/src
+cp -r {include,model,punter,punter2,solver,tests,CMakeLists.txt} /tmp/src
+
+tar -czvf icfp-$reg_id.tar.gz install PACKAGES README \
+-C bin punter \
+-C /tmp src/include src/model src/punter src/punter2 src/solver src/tests src/CMakeLists.txt
